6353a01ae0ba511ae8dac015dbb82efc997e2cec
Matteo C <matteochirico04@gmail.com>
2017-11-05 12:44:06 +0100

    fjernet

--- PasswordManager/Password Man/Password Man/Form_Main.cs
+++ PasswordManager/Password Man/Password Man/Form_Main.cs
@@ -14,7 +14,6 @@ namespace Password_Man
     public partial class Form_Main : Form
     {
         List<string> Users = new List<string>();
-        bool bastianFikserAlltid = true;
 
         public Form_Main()
         {
